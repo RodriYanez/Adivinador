730b40f502c9f4fe871d8910e52784ac6950e2fa
Rodrigo.Yanez <Rodrigo.Yanez@fisglobal.com>
2018-04-26 16:20:04 -0300

    La maquina adivina el numero finalizado.

--- app/src/main/java/com/example/rodrigoyanez/adivinador/AdivinaPCActivity.java
+++ app/src/main/java/com/example/rodrigoyanez/adivinador/AdivinaPCActivity.java
@@ -1,8 +1,11 @@
 package com.example.rodrigoyanez.adivinador;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import org.w3c.dom.Text;
@@ -15,18 +18,55 @@ public class AdivinaPCActivity extends AppCompatActivity {
     private int numeroActual, maxActual, minActual;
 
     private TextView numero;
-    private Button btnMayor, btnMenor, btnIgual;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_adivina_pc);
 
-        numero = (TextView) findViewById(R.id.numero);
-        btnIgual = (Button) findViewById(R.id.btnIgual);
-        btnMayor = (Button) findViewById(R.id.btnMayor);
-        btnMenor = (Button) findViewById(R.id.btnMenor);
+        numero = findViewById(R.id.numero);
+        Button btnIgual = findViewById(R.id.btnIgual);
+        Button btnMayor = findViewById(R.id.btnMayor);
+        Button btnMenor = findViewById(R.id.btnMenor);
 
         numeroActual = NUMERO_MAXIMO / 2;
+        maxActual = NUMERO_MAXIMO;
+        minActual = NUMERO_MINIMO;
         numero.setText(numeroActual + "?");
+
+        btnMayor.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                minActual = numeroActual;
+                numeroActual = (maxActual + numeroActual) / 2;
+                numero.setText(numeroActual + "?");
+            }
+        });
+        btnMenor.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                maxActual = numeroActual;
+                numeroActual = (minActual + numeroActual) / 2;
+                numero.setText(numeroActual + "?");
+            }
+        });
+        btnIgual.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                setContentView(R.layout.winner_screen);
+                Button btnVolverAJugar = findViewById(R.id.btnVolverAJugar);
+                TextView txtNumeroGanador = findViewById(R.id.txtNumeroGanador);
+
+                txtNumeroGanador.setText("El numero que pensaste es " + numeroActual);
+
+                btnVolverAJugar.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        Intent intent = new Intent(AdivinaPCActivity.this, AdivinaPCActivity.class);
+                        AdivinaPCActivity.this.startActivity(intent);
+                    }
+                });
+            }
+        });
     }
 }
